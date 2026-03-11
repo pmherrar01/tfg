5e4575bbce9f65910bca5f9ac22598191b3c69bd
pmherrar01 <pmherrerar01@educarex.es>
2026-03-11 08:13:53 +0000

    g

--- controllers/carritoController.php
+++ controllers/carritoController.php
@@ -1,8 +1,11 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['accion']) && $_POST['accion'] == 'agregar') {
     
+    // 1. Recogemos los datos del formulario
     $idPrenda = $_POST['idPrenda'];
     $talla = $_POST['talla'];
     $color_id = $_POST['color_id'];
@@ -23,7 +26,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['accion']) && $_POST['a
     exit;
 }
 
-header("Location: ../catalogo.php");
-exit;
-
+$carritoActual = isset($_SESSION['carrito']) ? $_SESSION['carrito'] : [];
 ?>
