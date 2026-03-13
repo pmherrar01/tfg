6da591058e2997509a726d574b6704428eb07055
pmherrar01 <pmherrerar01@educarex.es>
2026-03-13 13:21:20 +0000

    e

---
+++ models/pedido.php
@@ -0,0 +1,82 @@
+<?php
+
+class Pedido{
+    private $conexionDataBase;
+    private $idPedido;
+    private $idUsuario;
+    private $total;
+
+    public function __construct($db) {
+        $this->conexionDataBase = $db;
+    }
+
+    
+
+
+    /**
+     * Get the value of idPedido
+     */ 
+    public function getIdPedido()
+    {
+        return $this->idPedido;
+    }
+
+    /**
+     * Set the value of idPedido
+     *
+     * @return  self
+     */ 
+    public function setIdPedido($idPedido)
+    {
+        $this->idPedido = $idPedido;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idUsuario
+     */ 
+    public function getIdUsuario()
+    {
+        return $this->idUsuario;
+    }
+
+    /**
+     * Set the value of idUsuario
+     *
+     * @return  self
+     */ 
+    public function setIdUsuario($idUsuario)
+    {
+        $this->idUsuario = $idUsuario;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of total
+     */ 
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * Set the value of total
+     *
+     * @return  self
+     */ 
+    public function setTotal($total)
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+
+    public function crearPedido($idUsu, $total){
+        $sql = "INSER INTO pedidos";
+    }
+
+}
+
+?>
