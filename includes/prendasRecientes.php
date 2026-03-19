0dbbb0b40cf45bb63e66d3e2ba78f79233438496
pmherrar01 <pmherrerar01@educarex.es>
2026-03-19 10:26:20 +0000

    r

--- includes/prendasRecientes.php
+++ includes/prendasRecientes.php
@@ -1,4 +1,4 @@
-<section id="seccion-recientes" class="container my-5 d-none position-relative">
+<section id="seccionRecientes" class="container my-5 d-none position-relative">
     <h3 class="fw-bold text-uppercase mb-4" style="letter-spacing: 2px;">Visto Recientemente</h3>
     
     <button class="btn position-absolute top-50 start-0 translate-middle-y bg-white rounded-circle shadow-sm d-none d-md-block" 
@@ -7,7 +7,7 @@
         <i class="bi bi-chevron-left fs-5"></i>
     </button>
 
-    <div id="carrusel-recientes" class="row flex-row flex-nowrap overflow-auto pb-3" style="scrollbar-width: none; scroll-behavior: smooth;">
+    <div id="carruselRecientes" class="row flex-row flex-nowrap overflow-auto pb-3" style="scrollbar-width: none; scroll-behavior: smooth;">
         </div>
 
     <button class="btn position-absolute top-50 end-0 translate-middle-y bg-white rounded-circle shadow-sm d-none d-md-block" 
